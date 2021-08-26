e3e6d2219491f6b682b14088dfc9af1027198f52
frontcat <frontcatbackend@gmail.com>
2021-08-26 19:28:30 +1200

    Post Form done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { useRef, useState, useMemo } from "react";
+import React, { useState, useMemo } from "react";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import MySeclect from "./components/UI/select/MySeclect";
 import styles from "./styles/styles.css";
 
 function App() {
@@ -11,33 +9,27 @@ function App() {
     { id: 1, title: "The Terminator", body: "Shwarznegger, Michael Biehn" },
     { id: 2, title: "Rembo", body: "Stallone" },
     { id: 3, title: "The Green Elephant", body: "Epifancev" },
-  ]); //массив постов
+  ]); 
 
-
-
-  const [selectedSort, setSelectedSort] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
+const [filter, setFilter] = useState({sort:'', query:''}) //init
 
   const sortedPosts = useMemo(() => {
     console.log("it is working");
-    if (selectedSort) {
+    if (filter.sort) {
       return [...posts].sort((a, b) =>
-        a[selectedSort].localeCompare(b[selectedSort])
+        a[filter.sort].localeCompare(b[filter.sort])
       );
     }
     return posts;
-  }, [selectedSort, posts]);
+  }, [filter.sort, posts]);
 
   const sortedAndSearchedPosts = useMemo(() => {
     return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(searchQuery) || post.body.toLowerCase().includes(searchQuery)    
+      post.title.toLowerCase().includes(filter.query) || post.body.toLowerCase().includes(filter.query)    
     );
     
-  }, [searchQuery, sortedPosts]);
+  }, [filter.query, sortedPosts]);
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-  };
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -51,37 +43,14 @@ function App() {
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: "15px" }} />
-      <div>
-        <MyInput
-          placeholder="Search . ."
-          value={searchQuery}
-          onChange={event => setSearchQuery(event.target.value)}
-        />
-        <MySeclect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="Sorting By .."
-          options={[
-            {
-              value: "title",
-              name: "By name",
-            },
-            {
-              value: "body",
-              name: "By description",
-            },
-          ]}
-        />
-      </div>
-      {sortedAndSearchedPosts.length !== 0 ? (
+     <PostFilter filter={filter} setFilter={setFilter} />
+     
         <PostList
           remove={removePost}
           posts={sortedAndSearchedPosts}
           title="Movies List"
         />
-      ) : (
-        <div style={{ textAlign: "center" }}>There are Tickets</div>
-      )}
+  
     </div>
   );
 }
--- src/components/PostFilter.jsx
+++ src/components/PostFilter.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
+import MyInput from "./UI/input/MyInput";
+import MySeclect from "./UI/select/MySeclect";
 
-const PostFilter = ({PostFilter, setFilter}) => {
-    return (
-        <div>
-             <MyInput
-          placeholder="Search . ."
-          value={searchQuery}
-          onChange={event => setSearchQuery(event.target.value)}
-        />
-        <MySeclect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="Sorting By .."
-          options={[
-            {
-              value: "title",
-              name: "By name",
-            },
-            {
-              value: "body",
-              name: "By description",
-            },
-          ]}
-        />
-        </div>
-    );
+const PostFilter = ({filter, setFilter}) => {
+  return (
+    <div>
+      <MyInput
+        placeholder="Search . ."
+        value={filter.query}
+        onChange={(event) => setFilter({...filter, query: event.target.value})}
+      />
+      <MySeclect
+        value={filter.sort}
+        onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+        defaultValue="Sorting By .."
+        options={[
+          {
+            value: "title",
+            name: "By name",
+          },
+          {
+            value: "body",
+            name: "By description",
+          },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default PostFilter;
